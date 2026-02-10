9e45c0304c909c38cd758f1072429e9882c81780
Nilton Pimentel <63605485+niltonpimentel02@users.noreply.github.com>
2026-02-09 21:30:28 -0300

    Fix formatting and remove extra spaces in one_more_ugly

---
+++ ugly_code.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import json
+from collections import OrderedDict
+
+def   badly_formatted_function(  x,y,z   ):
+    unused_var = 42
+    result=x+y+z
+    if result==0:
+        return   True
+    else:
+        return False
+
+class   MyClass:
+    def __init__(self,name,   value):
+        self.name=name
+        self.value=value
+
+    def get_name( self ):
+        return   self.name
+
+import pathlib
+
+def another_function():
+    x = [1,2,3,4,5]
+    y = dict(a=1,b=2,c=3)
+    return x,y
+
+def   one_more_ugly(  a,b ):
+    x=a+b
+    return    x
